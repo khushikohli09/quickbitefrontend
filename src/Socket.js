570e739503437c530007d8728582318c2ec7f0f8
khushikohli09 <157889269+khushikohli09@users.noreply.github.com>
2026-02-15 11:56:18 +0530

    Update Socket.js

--- src/Socket.js
+++ src/Socket.js
@@ -1,7 +1,8 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000"); // change to your backend URL
+export const socket = io("https://quickbite-backend-47wd.onrender.com"); // change to your backend URL
 export const registerSocketUser = (userId, role) => {
   socket.emit("register", { userId, role });
 };
+
