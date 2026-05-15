cfcbf19e266624908823ace6f7966ea2d1bd6511
khushikohli09 <157889269+khushikohli09@users.noreply.github.com>
2026-05-15 17:53:31 +0530

    Delete src/Socket.js

--- src/Socket.js
+++
@@ -1,8 +0,0 @@
-// src/socket.js
-import { io } from "socket.io-client";
-
-export const socket = io("https://quickbite-backend-47wd.onrender.com"); // change to your backend URL
-export const registerSocketUser = (userId, role) => {
-  socket.emit("register", { userId, role });
-};
-
