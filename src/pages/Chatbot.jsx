725101696081dafb3d7b5f7dee5daaced696c226
khushikohli09 <157889269+khushikohli09@users.noreply.github.com>
2026-07-15 12:25:58 +0530

    Update Chatbot.jsx

--- src/pages/Chatbot.jsx
+++ src/pages/Chatbot.jsx
@@ -19,7 +19,7 @@ const Chatbot = () => {
         localStorage.getItem("token");
 
       const res = await axios.post(
-        "http://localhost:5000/api/chat",
+        `${process.env.REACT_APP_BACKEND_URL}/api/chat`
         { message },
         {
           headers: {
